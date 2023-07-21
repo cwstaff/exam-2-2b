8ba1a83d028d44806a1db2a83c8a08552fd43bc9
cwstaff <136363212+cwstaff@users.noreply.github.com>
2023-07-20 23:14:14 -0500

    Update handshake.cpp

--- handshake.cpp
+++ handshake.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
-// include additional necessary headers
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
-void query(/*add necessary parameters*/) {
-    // Should print: the print number (starting from 0), "SYN", and the three dots "..."
-}
-
-void response(/*add necessary parameters*/) {
-    // Should print "ACK"
+std::mutex mtx;
+std::condition_variable cv;
+bool is_syn_printed = false;
+void query(int count) {
+    for (int i = 0; i < count; ++i) {
+        std::unique_lock<std::mutex> lock(mtx);
+        while (is_syn_printed) {
+            cv.wait(lock);
+        }
+        // print statement
+        std::cout << "[" << i << "] SYN ... ";
+        is_syn_printed = true;
+        cv.notify_one();
+    }
+void response(int count) {
+    for (int i = 0; i < count; ++i) {
+        std::unique_lock<std::mutex> lock(mtx);
+        while (!is_syn_printed)
+            cv.wait(lock);
+        
+        // Print statement
+        std::cout << "ACK" << std::endl;
+        is_syn_printed = false;
+        cv.notify_one();
+    }
 }
 
 int main(int argc, char** argv) {
@@ -15,6 +36,8 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
+    // 1
+    int count = std::stoi(argv[1])
     /**
      * Steps to follow:
      * 1. Get the "count" from cmd args
@@ -23,6 +46,14 @@ int main(int argc, char** argv) {
      * 4. Provide the threads with necessary args
      * 5. Update the "query" and "response" functions to synchronize the output
     */
-   
+
+    // 2 - 4
+    std::thread t1(query, count);
+    std::thread t2(response, count);
+
+    // 5
+    t1.join();
+    t2.join();
+    
     return 0;
 }
